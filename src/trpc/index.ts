5c8874b130dff827f14deb3693850de059f9b318
Ashish Thomas <ashishthomas2202@gmail.com>
2023-12-27 14:36:33 -0500

    added new user collection

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,9 +1,11 @@
+import { authRouter } from "./auth-router";
 import { router, publicProcedure } from "./trpc";
 
 export const appRouter = router({
   anyApiRoute: publicProcedure.query(() => {
     return "Hello World";
   }),
+  auth: authRouter,
 });
 
 export type AppRouter = typeof appRouter;
